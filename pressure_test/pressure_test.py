640c1cff47bacd60f8cd913cc1e2faa5c80a7c15
Artrix <Artrix@Artrix.tech>
2018-05-22 23:18:41 +0800

    [Front]UI Compatible Fix

--- pressure_test/pressure_test.py
+++ pressure_test/pressure_test.py
@@ -12,9 +12,12 @@ def check(url, batch_index, request_index):
     try:
         start_t = time.time()
         response = requests.get(url, timeout=12).text
-        if len(response) > 30:
+        if len(response) > 30 and "Artrix" in response:
             print("[" + str(int((time.time() - start_t) * 100) / 100) + "s]batch" + str(batch_index) + "-" + str(
                 request_index) + ":OK")
+        else:
+            print("##[" + str(int((time.time() - start_t) * 100) / 100) + "s]batch" + str(batch_index) + "-" + str(
+                request_index) + ":BAD##")
     except requests.exceptions.ReadTimeout:
         print("##[" + str(int((time.time() - start_t) * 100) / 100) + "s]batch" + str(batch_index) + "-" + str(
             request_index) + ":Timeout##")
