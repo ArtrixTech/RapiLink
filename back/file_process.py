0d28c6450ffedd505c33a460cf6ea771b512fea9
Artrix <Artrix@Artrix.tech>
2018-05-24 23:29:13 +0800

    [Back Add]Directory Managing System [Front Add]Batch ID generating system

---
+++ back/file_process.py
@@ -0,0 +1,17 @@
+import os
+
+
+def get_save_location(file_name, batch_id):
+    """
+    - Generate the proper directory and return the location
+    :param file_name: Name of the file
+    :param batch_id: Batch ID to generate the directory
+    :return: Location for saving file. The path will be relative to the current working path.
+    """
+    if not os.path.exists("saved_files"):
+        os.makedirs("saved_files")
+
+    if not os.path.exists("saved_files\\" + batch_id):
+        os.makedirs("saved_files\\" + batch_id)
+
+    return "saved_files\\" + batch_id + "\\" + file_name
