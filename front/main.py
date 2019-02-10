1c017e16b292c823713a907dc2d49ffeba409eb5
ArtrixTech <Artrix@artrix.tech>
2019-02-10 20:57:01 +0800

    [Back] Bug fix

--- front/main.py
+++ front/main.py
@@ -11,8 +11,8 @@ from logger.log import Logger
 
 from bing_image.bing_image import get_bing_img_small
 
-front_blueprint = Blueprint('front', __name__, template_folder="templates", static_folder="static", subdomain="old")
-www_jump_blueprint = Blueprint('jump', __name__, template_folder="templates", static_folder="static", subdomain="www")
+front_blueprint = Blueprint('front_old', __name__, template_folder="templates", static_folder="static", subdomain="old")
+www_jump_blueprint = Blueprint('jump_old', __name__, template_folder="templates", static_folder="static", subdomain="www")
 
 debug_logger = Logger(logger_name="frontend_main")
 
