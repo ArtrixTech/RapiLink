50e3266454c8a7f077d01f1a860b1c4185d6218d
ArtrixTech <Artrix@artrix.tech>
2019-02-24 13:07:08 +0800

    [Front] Swiper load bug fix

--- mobile_new/static/js/animations/animations.js
+++ mobile_new/static/js/animations/animations.js
@@ -1,65 +1,54 @@
-var VerticalSwiper, HorizontalSwiper;
-$(document).ready(function () {
+var VerticalSwiper, HorizontalSwiper_Main, HorizontalSwiper_Help;
+//$(document).ready(function () {
+less.pageLoadFinished.then(
+    function () {
 
-    function work() {
-        VerticalSwiper = new Swiper('.swiper-container-vertical', {
-            parallax : true,
-            direction: 'vertical',
-            loop: false,
-            initialSlide: 1,
-            fadeEffect: {
-                crossFade: true,
-            },
-            observer: true,
-            pagination: {
-                el: '.swiper-pagination',
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            }
-        });
+        function work() {
+            window.VerticalSwiper = new Swiper('#swiper_container_vertical', {
+                parallax: false,
+                direction: 'vertical',
+                loop: false,
+                initialSlide: 1,
+                fadeEffect: {
+                    crossFade: true,
+                },
+                observer: true,
+                pagination: {
+                    el: '.swiper-pagination',
+                },
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
+            });
 
-        HorizontalSwiper_Main = new Swiper('.swiper-container-horizontal.main', {
-            parallax : true,
-            direction: 'horizontal',
-            loop: false,
-            initialSlide: 0,
-            parallax : true,
-            fadeEffect: {
-                crossFade: true,
-            },
-            observer: true,
-            navigation: {
-                nextEl: '.layui-icon-right',
-                prevEl: '.layui-icon-left',
-            }
-        });
+            window.HorizontalSwiper_Main = new Swiper('#swiper_container_horizontal_main', {
+                parallax: false,
+                direction: 'horizontal',
+                loop: false,
+                initialSlide: 0,
+                observer: true,
+                observerParent: true,
+                navigation: {
+                    nextEl: '.layui-icon-right',
+                    prevEl: '.layui-icon-left',
+                },
+            });
 
-        HorizontalSwiper_Help = new Swiper('.swiper-container-horizontal.help', {
-            parallax : true,
-            direction: 'horizontal',
-            loop: false,
-            initialSlide: 0,
-            fadeEffect: {
-                crossFade: true,
-            },
-            observer: true,
-            navigation: {
-                nextEl: '.layui-icon-right',
-                prevEl: '.layui-icon-left',
-            }
-        });
-
+            window.HorizontalSwiper_Help = new Swiper('#swiper_container_horizontal_help', {
+                parallax: false,
+                direction: 'horizontal',
+                loop: false,
+                initialSlide: 0,
+                observer: true,
+                observerParent: true,
+                navigation: {
+                    nextEl: '.layui-icon-right',
+                    prevEl: '.layui-icon-left',
+                },
+            });
+        }
+        setTimeout(work, 10);
     }
 
-    setTimeout(work, 128);
-    setTimeout(updateSwiper, 400);
-    setTimeout(updateSwiper, 800);
-    setTimeout(updateSwiper, 1200);
-    setTimeout(updateSwiper, 1600);
-    setTimeout(updateSwiper, 2000);
-
-});
-
-
+);
